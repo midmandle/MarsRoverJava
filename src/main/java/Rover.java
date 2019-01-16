98fdba5bd3d8b288fc8ffeb13c43e4fd9393f239
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-01-16 11:26:03 +0000

    Green: Rover is initialised to start at location 0,0 facing North

---
+++ src/main/java/Rover.java
@@ -0,0 +1,5 @@
+public class Rover {
+    public String determineCurrentLocation() {
+        return "0,0,N";
+    }
+}
---
+++ src/test/java/RoverStartsAt00NTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class RoverStartsAt00NTest {
+    @Test
+    public void rover_starts_at_location_00_facing_north() {
+        Rover rover = new Rover();
+        assertEquals("0,0,N", rover.determineCurrentLocation());
+    }
+}
