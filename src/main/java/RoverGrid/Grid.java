ba847d6f6b8b8305e667c4a49b7c3054d85fe3eb
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-01-17 15:35:33 +0000

    Refactor: extracted Grid logic for wrapping to top, bottom, left and right.

--- src/main/java/RoverGrid/Grid.java
+++ src/main/java/RoverGrid/Grid.java
@@ -16,22 +16,43 @@ public class Grid {
     }
 
     public Coordinate determineVehiclesNextValidLocation(VehicleState roverVehicleState) throws Exception {
-        Coordinate nextCoordinates = this.getNextLocationInVehiclesDirection(roverVehicleState);
-
-        if(nextCoordinates.xCoordinate > this.width)
-            nextCoordinates = new Coordinate(0, nextCoordinates.yCoordinate);
-        if(nextCoordinates.yCoordinate > this.height)
-            nextCoordinates = new Coordinate(nextCoordinates.xCoordinate, 0);
-        if(nextCoordinates.xCoordinate < 0)
-            nextCoordinates = new Coordinate(this.width, nextCoordinates.yCoordinate);
-        if(nextCoordinates.yCoordinate < 0)
-            nextCoordinates = new Coordinate(nextCoordinates.xCoordinate, this.height);
+        Coordinate nextCoordinates = generateNextValidCoordinate(roverVehicleState);
 
         if(this.hasObstacleAt(nextCoordinates)) {
             throw new Exception("Obstacle ahead!");
         }
 
         return nextCoordinates;
+    }
+
+    private Coordinate generateNextValidCoordinate(VehicleState roverVehicleState) {
+        Coordinate nextCoordinates = this.getNextLocationInVehiclesDirection(roverVehicleState);
+
+        if(nextCoordinates.xCoordinate > this.width)
+            nextCoordinates = wrapToLeftSide(nextCoordinates);
+        if(nextCoordinates.yCoordinate > this.height)
+            nextCoordinates = wrapToBottomSide(nextCoordinates);
+        if(nextCoordinates.xCoordinate < 0)
+            nextCoordinates = wrapToRightSide(nextCoordinates);
+        if(nextCoordinates.yCoordinate < 0)
+            nextCoordinates = wrapToTopSide(nextCoordinates);
+        return nextCoordinates;
+    }
+
+    private Coordinate wrapToTopSide(Coordinate nextCoordinates) {
+        return new Coordinate(nextCoordinates.xCoordinate, this.height);
+    }
+
+    private Coordinate wrapToRightSide(Coordinate nextCoordinates) {
+        return new Coordinate(this.width, nextCoordinates.yCoordinate);
+    }
+
+    private Coordinate wrapToBottomSide(Coordinate nextCoordinates) {
+        return new Coordinate(nextCoordinates.xCoordinate, 0);
+    }
+
+    private Coordinate wrapToLeftSide(Coordinate nextCoordinates) {
+        return new Coordinate(0, nextCoordinates.yCoordinate);
     }
 
     public void addObstacleAt(Coordinate obstacleCoordinate) {
